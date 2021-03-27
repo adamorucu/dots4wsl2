7d235f233218277f07a769ac22c6c5c7fd531a29
posterrieri <adamdorucu@gmail.com>
2021-03-27 19:24:32 +0300

    small changes

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # Installations
+echo "Upgrade..."
 apt upgrade
+echo "Installs..."
 apt-get install python3-pip
 pip3 -y install numpy matplotlib pandas notebook
 apt-get install tmux
 
 # Symlinks
-echo "Stating Symlinking"
+echo "Symlinking..."
 ln -sf $(pwd)/.jupyter ~/.jupyter
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.vim ~/.vim
 ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
 
 # Intall packages
+echo "Vim installs..."
 vim -c PlugInstall
